db8ab4d1f476180b2dcf7fdfe6ef9e7c44ae1b41
adeluthfi07 <adeluthfi0710@gmail.com>
2020-10-14 15:46:40 +0700

    dasboard

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -10,7 +10,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard v3</li>
+                    <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
